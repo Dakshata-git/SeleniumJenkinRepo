4e905740360b4715a4b460560b423f791a668f19
Dakshata-git <dakshata.3012@gmail.com>
2021-02-12 16:08:03 +0200

    bat file updates

--- TestNGBatchFile.bat
+++ TestNGBatchFile.bat
@@ -1,5 +1,7 @@
 set projectLocation=C:\SeleniumJenkinProject
 cd\
 cd %projectLocation%
-set classpath=%projectLocation%\bin;%projectLocation%\lib\*;
+set classpath=projectLocation/bin
+set classpath=%classpath%;projectLocation/lib/*
 java org.testng.TestNG testng.xml
+
